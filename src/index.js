9a017ddba5499bc6543e2c6e21c16781be4ba80f
Kimanthif <kimanthi.felistus@gmail.com>
2024-10-14 22:22:02 +0300

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,40 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityELement = document.querySelector("#city");
+  let descriptionELement = document.querySelector("#description");
+  let humidityELement = document.querySelector("#humidity");
+  let windSpeedELement = document.querySelector("#wind-speed");
+  let timeELement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityELement.innerHTML = response.data.city;
+
+  timeELement.innerHTML = formatDate(date);
+  descriptionELement.innerHTML = response.data.condition.description;
+  humidityELement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedELement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
